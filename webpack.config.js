0ac6cdb6387523acacd57ae193b199a613f6d80a
Amin <m_amin_a@msn.com>
2017-05-17 15:08:18 +0800

    ES-Master updated my own webpack and also updated the package.json to the latest dependency updates

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,31 @@
 module.exports = {
-  entry: ['babel-polyfill', './app/index.js'],
-  output: {
-    path: './build',
-    // if the above line does not work, try `path: __dirname + '/build'`
-    filename: 'bundle.js'
-  },
-  module: {
-    loaders: [
-      {
-        test: /\.js$/, // a regular expression that catches .js files
-        exclude: /node_modules/,
-        loader: 'babel-loader'
-      }
-    ]
-  },
-  devServer: {
-    port: 3000, // most common port
-    contentBase: './build',
-    inline: true
-  }
+    //entry point for the application
+    entry: ['./app/index.js'],
+
+    //For output we are going to target bundle.js file which index.html is refering to
+    //So when webpack converts index.js then it is going to move all the code from index.js to bundle.js
+    output :{
+        path: __dirname + '/build',
+        filename: 'bundle.js'
+        //Please not that __dirname is necessary otherwise it won't work. It seems it is related to the version of webpack
+    },
+    module: {
+        //We specify objects for each loader
+        loaders: [
+            {
+                loader: 'babel-loader', //it must know what files it should be looking for. \.js$ means look for all js files in the project
+                //but we do not want all js files in the node_modules so we will exlude some
+                test: /\.js$/,
+                exclude: /node_modules/
+            }
+        ]
+    },
+    devServer: {
+        //It has 3 properties
+        port: 3000,
+        contentBase: './build', //our build folder
+        inline: true //allows us to run automatic live code updating
+
+    }
+    
 }
